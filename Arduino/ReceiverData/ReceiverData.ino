7250dc39bf8d230e6c24779ca7e156e1d3290a42
jsorensen12 <39261296+jsorensen12@users.noreply.github.com>
2018-05-21 13:29:15 +0200

    Update ReceiverData.ino

--- Arduino/ReceiverData/ReceiverData.ino
+++ Arduino/ReceiverData/ReceiverData.ino
@@ -28,30 +28,30 @@ RF24 radio(7,8);  //set up nRF24L01 radio on spi bus pins 7 & 8
 
 const uint64_t pipes[1] = { 0x544d52LL };
 
-int dts = 4;    //size of data in byte for transfor
-byte data[4];
+int dts = 4;    //Size of data in byte for transfor
+byte data[4];   //Storing place for received data
 
 void setup() {
   Serial.begin(115200);
-  printf_begin();
+  printf_begin();   
 
   radio.begin();
-  radio.setChannel(100);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setDataRate(RF24_1MBPS);       //max 1Mbps if ACK ar disabled
+  radio.setChannel(100);    //Set the channel the modules communicat at
+  radio.setPALevel(RF24_PA_MAX);        //Set the power level in dB, MAX=0 dB, HIGH=-6 dB, LOW=-12 dB, MIN=-18 dB
+  radio.setDataRate(RF24_1MBPS);       //max 1Mbps if ACK ar disabled, Settings 1MBPS or 250KBPS
   radio.setAutoAck(0);                 //dissable ACK if value = '0'
-  radio.disableDynamicPayloads();
+  radio.disableDynamicPayloads();     //Set the transmission size to dynamic
   radio.setPayloadSize(dts);           //chagnes the bytesize of the payload (from 1 to 32)
   radio.disableCRC();
-  radio.openReadingPipe(1,pipes[0]);   
-  radio.startListening();
-  radio.powerUp();
+  radio.openReadingPipe(1,pipes[0]);    //Open the reading pipes   
+  radio.startListening();               //Set the module to receiverig
+  radio.powerUp();                      //Power up the modules
 }
 
 
 void loop() {
-  if(radio.available()){
-    radio.read(&data, dts);
+  if(radio.available()){      //Wait on received data
+    radio.read(&data, dts);   
     for(int i = 0; i<dts; i++){
       Serial.write(data[i]);
   }
