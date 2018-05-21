f73bf0285cf68ec2a7f836a95dfdbbdeb851f3a3
jsorensen12 <39261296+jsorensen12@users.noreply.github.com>
2018-05-21 13:43:33 +0200

    Update ReceiverDataTestBitError.ino

--- Arduino/Appendix/ReceiverDataTestBitError/ReceiverDataTestBitError.ino
+++ Arduino/Appendix/ReceiverDataTestBitError/ReceiverDataTestBitError.ino
@@ -27,57 +27,46 @@ RF24 radio(7, 8); //set up nRF24L01 radio on spi bus pins 7 & 8
 /******************************************/
 
 const uint64_t pipes[2] = { 0xABCDABLL, 0x544d52LL };
-/*0xABCDABCD71LL  544d52687CLL*/
-int dts = 4;    //size of data in byte for transfor
-byte data[4];
-unsigned long startTime, StopTime;
-unsigned long counter = 0;
-unsigned long BitError = 0;
-unsigned long packetError = 0;
-unsigned long byteError = 0;
-byte myVar[4];
-byte value[4];
-unsigned long packetcount = 0;
+int dts = 4;        //Size of data in byte for transfer
+byte data[4];     //Storing place for the received data
+unsigned long startTime, StopTime;  //The timer for the printing out results
+unsigned long counter = 0;    //Total bit error counter for each packet
+unsigned long BitError = 0;   //Counts the packets with only one bit error
+unsigned long packetError = 0;  //Counts the packets with more than one bit error
+unsigned long byteError = 0;    //Counts the errors in each byte
+byte myVar[4];  //The right received bytes to check the received bytes
+byte value[4];    //The array where the different between the received bytes and check bytes
+unsigned long packetcount = 0;  //Counts the packets received
 
 void setup() {
-  myVar[0] = 0xAA;
-  myVar[1] = 0xAA;
-  myVar[2] = 0xE6;
-  myVar[3] = 0x15;
-  //data[0] = 0xAA;
-  //data[1] = 0xAA;
-  //data[2] = 0xE6;
-  //data[3] = 0x15;
+  myVar[0] = 0xAA;  //The check byte
+  myVar[1] = 0xAA;  //The check byte
+  myVar[2] = 0xE6;  //The check byte
+  myVar[3] = 0x15;  //The check byte
   
   Serial.begin(115200);
-  printf_begin();
-  //DDRD = DDRD | B01000000;
-  Serial.flush();
-
-  radio.begin();
-  radio.setChannel(100);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.setDataRate(RF24_1MBPS);     //max 1Mbps if ACK ar disabled
-  radio.setAutoAck(0);               //dissable ACK if value = '0'
-  radio.disableDynamicPayloads();
-  radio.setPayloadSize(dts);         //chagnes the bytesize of the payload (from 1 to 32)
-
+  Serial.flush();   //Flush the buffer each time the Arduino starts up again
+  radio.begin();    //Starts the radio programming
+  radio.setChannel(100);  //Set the channel the modules communicate at 
+  radio.setPALevel(RF24_PA_MAX);  //Set the power level
+  radio.setDataRate(RF24_1MBPS);     //Max 1Mbps if ACK is disabled
+  radio.setAutoAck(0);               //Disable ACK if value = '0'
+  radio.disableDynamicPayloads(); //Set the transmission size to dynamic
+  radio.setPayloadSize(dts);         //Changes the byte size of the payload (from 1 to 32)
   radio.disableCRC();
-  radio.openWritingPipe(pipes[0]);     //
-  radio.openReadingPipe(1, pipes[1]);  //
-
-  radio.startListening();
-  radio.powerUp();
+  radio.openWritingPipe(pipes[0]);     //Opening the writing pipe
+  radio.openReadingPipe(1, pipes[1]);  //Opening the reading pipes
+  radio.startListening(); //Set the module to receiving
+  radio.powerUp();  //Start up the modules
   startTime = millis();
 }
-
 
 void loop() {
   if (radio.available()) {
     packetcount++;
-    radio.read(&data, dts);
+    radio.read(&data, dts);   //Receiving data and save it in to the storage array
     for(int i = 0; i<dts; i++){
-      value[i] = myVar[i] ^ data[i];
+      value[i] = myVar[i] ^ data[i];  //Check for different in the received data and checking byte
       if(value[i] == 0x01){
         counter++;
       }
@@ -103,7 +92,7 @@ void loop() {
         counter++;
       }
       if(value[i] != 0x00 && value[i] !=0x01 && value[i] !=0x02 && value[i] !=0x04 && value[i] !=0x08 && value[i] !=0x10 && value[i] !=0x20 && value[i] !=0x40 && value[i] !=0x80 ){
-        byteError++;
+        byteError++;  //Checks for more bit errors in the bytes
       }
     }
     if(counter == 1 && byteError == 0){
@@ -114,18 +103,15 @@ void loop() {
       packetError++;
       counter = 0;
     }
-    
  }
  StopTime = millis();
-  if (StopTime-startTime >= 10000){
+  if (StopTime-startTime >= 10000){   //Set the time to print out results
     Serial.print("Packets with 1 Error: ");
     Serial.println(BitError);
     Serial.print("packet with 2 or more errors: ");
     Serial.println(packetError);
     Serial.print("Packet received ");
     Serial.println(packetcount);
-
-    counter = 0;
     BitError = 0;
     packetError = 0;
     packetcount = 0;
