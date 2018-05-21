fb0bbff69494b5cf8249bce53cccf4fd7e848b33
godske2 <22340441+godske2@users.noreply.github.com>
2018-05-21 12:19:05 +0200

    Update Receiver_Code.py

--- RaspberryPi/Full_system_simulation RPI/Receiver_Code.py
+++ RaspberryPi/Full_system_simulation RPI/Receiver_Code.py
@@ -17,7 +17,8 @@ def GenCCITT_CRC16(Buffer):
       try:
               temp0  = ord(Buffer[byte])
       except:
-              print("fail to make CRC correctly")
+             l=1
+            #              print("fail to make CRC correctly")
       temp0  <<= 8
       crcsum ^= temp0
 
@@ -37,26 +38,13 @@ def check_CRC_sum(in1,in2,in3):
         buff=[]
         buff.append(in1)
         buff.append(in2)
-        tempo = '{:04x}'.format(GenCCITT_CRC16(buff))
-        if (in3.decode('charmap')).upper() ==(tempo).upper():
+        tempo = str('{:04x}'.format(GenCCITT_CRC16(buff)))
+        
+        if (str(in3)).upper() == str(tempo.encode('charmap')).upper():
                 return(True)
         else:
                 return(False)
         
-########Test code for crc################
-'''
-outputer=[b'']*9
-
-outputer[0] = b'\xff'
-outputer[1] = b'\xd8'
-outputer[2] = b'498a'
-outputer[3] = b'\xff'
-outputer[4] = b'\xff'
-outputer[5] = b'1d0f'
-outputer[6] = b'\xd9'
-outputer[7] = b'\x00'
-outputer[8] = b'afbf'
-'''
 #######Code to run#######################
 ser = serial.Serial("/dev/ttyS0",
                     baudrate=115200,
@@ -64,49 +52,69 @@ ser = serial.Serial("/dev/ttyS0",
                     timeout=0,
                     parity='N')
 
-outputer=[b'']*1000000
+outputer=[b'']*100000
 my_path = os.path.abspath(os.path.dirname(__file__)) # relative script path
 pathout = os.path.join(my_path+"/output", "output.jpg") # Output image
 a=0
 temp=b''
 
-
 f=False
 count = 0
 x=0
 checker = False
-while x <len(outputer):
-        outputer[x]=ser.read()
-        if outputer[x]==b'\xd9':
-                if(outputer[x-1]==b'\xff'):
-                        outputer[x+1]=ser.read()
-                        break
-                elif(outputer[x-2]==b'\xff'):
-                        outputer[x+1]=ser.read()
-                        outputer[x+2]=ser.read()
-                        x+=2
-                        break
-        x+=1
+print("Read values")
+temp=b''
+
+while True:
+        temp = ser.read()
+        if not temp == b'':
+#           print(temp)
+           outputer[x]=temp
+           if outputer[x]==b'\xd9':
+                   if(outputer[x-1]==b'\xff'):
+                           print("found ff d9")
+                           outputer[x+1]=ser.read()
+                           outputer[x+2]=ser.read()
+                           outputer[x+3]=ser.read()
+                           outputer[x+4]=ser.read()
+                           break
+                   elif(outputer[x-5]==b'\xff'):
+                           print("found ff d9")
+                           outputer[x+1]=ser.read()
+                           outputer[x+2]=ser.read()
+                           outputer[x+3]=ser.read()
+                           outputer[x+4]=ser.read()
+                           outputer[x+5]=ser.read()
+                           x+=3
+                           break
+           x+=1
 y=0
 check =False
+print("check crc")
+CRC_COUNT=0
 while True:
-        if y==(len(outputer)-((len(outputer))%3)):
+        if outputer[y]==b'\xff':
+           if outputer[y+1]==b'\xd9':
+              break
+           if outputer[y+5]==b'\xd9':
+              break
+        if y==(len(outputer)-((len(outputer))%6)):
                 break
-        check = check_CRC_sum(outputer[y],outputer[y+1],outputer[y+2])
+        j = str(outputer[y+2]+outputer[y+3]+outputer[y+4]+outputer[y+5])
+        check = check_CRC_sum(outputer[y],outputer[y+1],j)
         if check:
                 check=False
         elif not check:
-                print("WE HAVE A CRC FAIL! STOPPED PROGRAM AT runthrough#: {}".format(1+int(y/3)))
-                exit()
-        y+=3
-
+                CRC_COUNT +=1
+        y+=6
+s=0
 file = open(pathout,'w+b')
 while s < x:
-        file.write(chr(int.from_bytes(outputer[s],byteorder='big')).encode('charmap'))
+        file.write(chr(int.from_bytes(outputer[s],byteorder='big')).encode('charmap'))    
         file.write(chr(int.from_bytes(outputer[s+1],byteorder='big')).encode('charmap'))
-        s+=3
+        s+=6
 file.close()
-print("Done checking the stirngs for failures")
+print("Completed the cycle with: {}, CRC errors!".format(CRC_COUNT))
 
 
 
