f5c176b845385918df3869d59b06be0267211173
godske2 <22340441+godske2@users.noreply.github.com>
2018-05-21 12:19:43 +0200

    Update Sender_Code.py

--- RaspberryPi/Full_system_simulation RPI/Sender_Code.py
+++ RaspberryPi/Full_system_simulation RPI/Sender_Code.py
@@ -15,9 +15,10 @@ camera = PiCamera()
 camera.resolution=(512,512)
 camera.rotation =0
 camera.ISO = 100
-camera.exposure_mode='auto'
+camera.exposure_mode='beach'
 camera.contrast=0
 camera.sharpness=0
+camera.brightness=30
 
 my_path = os.path.abspath(os.path.dirname(__file__)) # relative script path
 
@@ -31,7 +32,8 @@ uncompressedpath = os.path.join(my_path+"/input",inpath[0])
 path = os.path.join(my_path+"/input", "W_Compressed.jpeg")
 
 
-compress=99  #how many percent to compress (100 - compress
+compress=75
+#how many percent to compress (100 - compress
 
 imag = Image.open(uncompressedpath) #original image open
 imag = imag.convert("RGB")
@@ -44,7 +46,7 @@ start=time.time()
 fileRead = open(path,'r+b')
 inputer = fileRead.read()
 fileRead.close()
-s=(math.ceil(len(inputer)/2))
+s=(math.ceil(len(inputer)))
 rawdata = [b'\x00']*(len(inputer)+s)
 for y in range(len(inputer)):
         rawdata[y] =bytes([int(inputer[y])])
@@ -80,28 +82,28 @@ def GenCCITT_CRC16(Buffer):
 buffer=[]
 temp=0
 if len(rawdata)%2:
-        outputer = [b'\xF0']*int(len(rawdata)+10)
+        outputer = [b'\xF0']*int(len(rawdata)+4)
         print("Upper")
 else:
-        outputer = [b'\x00']*int(len(rawdata)+10)
+        outputer = [b'\xFA']*int(len(rawdata)+4)
         print("lower")
 print(int((len(outputer))))
-print(type(outputer))
 #for x in range(int((len(rawdata)/2))):
 d=0
 f=0
+
 while len(rawdata)>d:
        buffer=[]
        buffer.append(rawdata[f])
        buffer.append(rawdata[f+1])
        temp= '{:04x}'.format(GenCCITT_CRC16(buffer))
-       outputer[d+2]=temp
+       g=str(temp)
+       outputer[d+2]=(str(g[0]+g[1])).encode()
+       outputer[d+3]=(str(g[2]+g[3])).encode()
        outputer[d]=rawdata[f]
        outputer[d+1]=rawdata[f+1]
-       d+=3
+       d+=4
        f+=2
-
-print(outputer)
 print("Loading, compressing and preparing to send takes: {}".format(time.time()-start))
 start = time.time()
 
@@ -117,20 +119,15 @@ ser = serial.Serial("/dev/ttyS0",
                     xonxoff=False)
 
 a=0
-'''
+
 for x in range(len(outputer)):
     ser.write(outputer[x])
 if( (len(outputer)%4)):
         for x in range(4-(len(outputer)%4)):
                 ser.write(b'\XFF')
                 a=x
-'''
+
 print("Time to transmit the image to the receiver: {}".format(time.time()-start))
 ser.close()
 print(len(outputer))
-print(a)
-print(len(outputer)%4)
-print(outputer[len(outputer)-1])
-print(outputer[len(outputer)-2])
-print(outputer[len(outputer)-3])
-print(outputer[len(outputer)-4])
+
