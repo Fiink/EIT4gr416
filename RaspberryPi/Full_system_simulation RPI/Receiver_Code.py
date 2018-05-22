1be0193ab573c548f65a2da92f9dda67b4b443a5
godske2 <22340441+godske2@users.noreply.github.com>
2018-05-22 11:37:34 +0200

    Update Receiver_Code.py

--- RaspberryPi/Full_system_simulation RPI/Receiver_Code.py
+++ RaspberryPi/Full_system_simulation RPI/Receiver_Code.py
@@ -4,6 +4,43 @@ import locale,time         #Encoding and timing
 import serial
 from PIL import Image
 
+
+
+error_correct =[4129,
+                8258,
+                16516,
+                33032,
+                4657,
+                9314,
+                18628,
+                37256,
+                13105,
+                26210,
+                52420,
+                35241,
+                883,
+                1766,
+                3532,
+                7064]
+
+error_correct_crc =[1,
+                    2,
+                    4,
+                    8,
+                    16,
+                    32,
+                    64,
+                    128,
+                    256,
+                    512,
+                    1024,
+                    2048,
+                    4096,
+                    8192,
+                    16384,
+                    32768]
+
+
 def GenCCITT_CRC16(Buffer):
    numBytes = len(Buffer)
    numBits  = 8
@@ -32,7 +69,20 @@ def GenCCITT_CRC16(Buffer):
             crcsum ^= polynom
    return(crcsum)
 
-
+def error_check(c, d):
+    cal_crc=int(str(d[0]+d[1]+d[2]+d[3]).encode('charmap'),16)
+    try:
+       input_crc =int(str(c[2]+c[3]+c[4]+c[5]).encode('charmap'),16)
+    except:
+       return(False)
+    in_crc=input_crc 
+    in_crc ^=cal_crc
+    for x in range(len(error_correct_crc)):
+        if in_crc == error_correct[x]:
+            return True
+        if in_crc == error_correct_crc[x]:
+            return True
+    return (False)
 
 def check_CRC_sum(in1,in2,in3):
         buff=[]
@@ -52,7 +102,7 @@ ser = serial.Serial("/dev/ttyS0",
                     timeout=0,
                     parity='N')
 
-outputer=[b'']*100000
+outputer=[b'']*10000000
 my_path = os.path.abspath(os.path.dirname(__file__)) # relative script path
 pathout = os.path.join(my_path+"/output", "output.jpg") # Output image
 a=0
@@ -62,12 +112,15 @@ f=False
 count = 0
 x=0
 checker = False
+
 print("Read values")
 temp=b''
 
 while True:
         temp = ser.read()
         if not temp == b'':
+           if (x==0):
+              start=time.time()
 #           print(temp)
            outputer[x]=temp
            if outputer[x]==b'\xd9':
@@ -88,25 +141,37 @@ while True:
                            x+=3
                            break
            x+=1
+print("It took: {} seconds to read the image".format(time.time()-start))
+start=time.time()
 y=0
 check =False
 print("check crc")
 CRC_COUNT=0
+error_count=0
 while True:
         if outputer[y]==b'\xff':
            if outputer[y+1]==b'\xd9':
               break
            if outputer[y+5]==b'\xd9':
               break
-        if y==(len(outputer)-((len(outputer))%6)):
+        if y==(x):
                 break
         j = str(outputer[y+2]+outputer[y+3]+outputer[y+4]+outputer[y+5])
         check = check_CRC_sum(outputer[y],outputer[y+1],j)
+        buff=[]
+        buff.append(outputer[y])
+        buff.append(outputer[y+1])       
+        if error_check(j,'{:04x}'.format(GenCCITT_CRC16(buff))):
+           error_count+=1
+        elif not error_check(j,'{:04x}'.format(GenCCITT_CRC16(buff))):
+           error_count=error_count
         if check:
                 check=False
         elif not check:
                 CRC_COUNT +=1
         y+=6
+print("It took: {} seconds to check the CRC".format(time.time()-start))
+start=time.time()
 s=0
 file = open(pathout,'w+b')
 while s < x:
@@ -114,8 +179,6 @@ while s < x:
         file.write(chr(int.from_bytes(outputer[s+1],byteorder='big')).encode('charmap'))
         s+=6
 file.close()
+print("It took: {} seconds to save the image".format(time.time()-start))
 print("Completed the cycle with: {}, CRC errors!".format(CRC_COUNT))
-
-
-
-
+print("out of the CRC erros, {}, could be error corrected!".format(error_count))
